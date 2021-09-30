b91253ea80e9ea047f7cbee0b559b0e46bd54b61
Kerem Demir <demirazizkerem@gmail.com>
2021-09-30 10:47:36 +0300

    Samples edited

---
+++ 16x2Lcd/LCD16x2_cfg.c
@@ -0,0 +1,20 @@
+/*
+ * LCD16x2_cfg.c
+ *
+ *  Created on: Jun 23, 2020
+ *      Author: Khaled Magdy
+ */
+
+#include "LCD16x2.h"
+
+const LCD16x2_CfgType LCD16x2_CfgParam =
+{
+	GPIOC,
+	GPIO_PIN_0,
+	GPIO_PIN_1,
+	GPIO_PIN_2,
+	GPIO_PIN_3,
+	GPIO_PIN_4,
+	GPIO_PIN_5,
+	80
+};
